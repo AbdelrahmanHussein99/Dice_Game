b803110b4c75c81f8647c076978c3e0dddbcb5eb
AbdelrahmanHussein21 <abdelrahmanhussain51@gmail.com>
2025-03-10 07:04:44 +0200

    add functionly new btn

--- src/JS/script.js
+++ src/JS/script.js
@@ -2,6 +2,8 @@
 //selecting elements
 const score0El = document.getElementById("score--0");
 const score1El = document.getElementById("score--1");
+const player0El = document.getElementById("player--0");
+const player1El = document.getElementById("player--1");
 const current0El = document.getElementById("current--0");
 const current1El = document.getElementById("current--1");
 const diceImg = document.querySelector("#dice-img");
@@ -10,37 +12,69 @@ const btnRoll = document.getElementById("btn--roll");
 const btnHold = document.getElementById("btn--hold");
 
 //starting conditions
-let currentScrore = 0;
-let scores = [0, 0];
-score0El.textContent = 0;
-score1El.textContent = 0;
-let activePlayer = 0;
-diceImg.classList.add("hidden-visb");
+let currentScrore, scores, activePlayer, playing;
+
+//----
+const init = function () {
+  currentScrore = 0;
+  scores = [0, 0];
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceImg.classList.add("hidden-visb");
+  player0El.classList.remove("opacity-50");
+  player0El.classList.remove("bg-gray-700");
+  player1El.classList.add("opacity-50");
+  player1El.classList.remove("bg-gray-700");
+};
+init();
+const changeActivePlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  player0El.classList.toggle("opacity-50");
+  player1El.classList.toggle("opacity-50");
+  currentScrore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+};
 
 btnRoll.addEventListener("click", function () {
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  diceImg.classList.remove("hidden-visb");
-  diceImg.src = `src/images/dice-${dice}.png`;
+    diceImg.classList.remove("hidden-visb");
+    diceImg.src = `src/images/dice-${dice}.png`;
 
-  if (dice !== 1) {
-    currentScrore += dice;
+    if (dice !== 1) {
+      currentScrore += dice;
 
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScrore;
-  } else {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScrore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScrore;
+    } else {
+      changeActivePlayer();
+    }
   }
 });
 
 btnHold.addEventListener("click", function () {
-  scores[activePlayer] += currentScrore;
-  document.getElementById(`score--${activePlayer}`).textContent =
-    scores[activePlayer];
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
-  currentScrore = 0;
+  if (playing) {
+    scores[activePlayer] += currentScrore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
 
-  activePlayer = activePlayer === 0 ? 1 : 0;
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      diceImg.classList.add("hidden-visb");
+      document
+        .getElementById(`player--${activePlayer}`)
+        .classList.add("bg-gray-700");
+      //.>>>
+    } else {
+      changeActivePlayer();
+    }
+  }
 });
+btnNew.addEventListener("click", init);
